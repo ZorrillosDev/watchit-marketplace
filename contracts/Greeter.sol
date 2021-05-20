48f19f8ab6e19706887ce998c2406c9b5267ec85
Mark Henderson <mark@mrh.io>
2021-05-20 15:25:36 -0400

    Initial commit, no tests, old name

---
+++ contracts/Greeter.sol
@@ -0,0 +1,48 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "hardhat/console.sol";
+
+
+contract MyCollectible is ERC1155, AccessControl {
+  bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  constructor() ERC1155("") {
+      _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+      _setupRole(URI_SETTER_ROLE, msg.sender);
+      _setupRole(MINTER_ROLE, msg.sender);
+  }
+
+  function setURI(string memory newuri) public {
+    require(hasRole(URI_SETTER_ROLE, msg.sender));
+    _setURI(newuri);
+  }
+
+  function mint(address account, uint256 id, uint256 amount, bytes memory data)
+    public
+  {
+    require(hasRole(URI_SETTER_ROLE, msg.sender));
+    _mint(account, id, amount, data);
+  }
+
+  function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+    public
+  {
+    require(hasRole(URI_SETTER_ROLE, msg.sender));
+    _mintBatch(to, ids, amounts, data);
+  }
+
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC1155, AccessControl)
+    returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
+  }
+}
