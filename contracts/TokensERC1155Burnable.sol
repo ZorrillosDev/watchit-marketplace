b4eb20c25b41dc04c195798540b4304e0a88b932
geolffrey <gmjun2000@gmail.com>
2021-06-13 11:27:42 -0600

    refactor: spliting code transferable

--- contracts/TokensERC1155Burnable.sol
+++ contracts/TokensERC1155Burnable.sol
@@ -3,9 +3,7 @@ pragma solidity ^0.8.0;
 import "./TokensERC1155NFT.sol";
 
 abstract contract Burnable is TokensERC1155NFT  {
-    function burn(address account, uint256 id, uint256 amount) public {
-        _burn(account, id, amount); //TODO
-    }
+
 
     function burnNFT(address account, uint256 id) public {
         bool isAdmin = hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -17,6 +15,5 @@ abstract contract Burnable is TokensERC1155NFT  {
     function burnBatchNFT(address account, uint256[] memory ids, uint256[] memory amounts) public {
         _burnBatch(account, ids, amounts); // TODO
     }
-
 
 }
