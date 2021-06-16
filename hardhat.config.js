f4de28f1a2381047c2bd1b3625629915d9346d26
Mark Henderson <henderson.mark@gmail.com>
2021-06-16 18:51:24 -0400

    adding ropsten config to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,15 @@
 /* global task, ethers */
 
+require('dotenv').config()
+
 require('@nomiclabs/hardhat-waffle')
 require('hardhat-gas-reporter')
 require('solidity-coverage')
 require('hardhat-contract-sizer')
 require('@openzeppelin/hardhat-upgrades')
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -36,5 +41,11 @@ module.exports = {
     currency: 'EUR',
     showTimeSpent: true,
     coinmarketcap: '6ffc3d5b-865e-482d-a05c-144ba7fe319e'
+  },
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
   }
 }
