5bfb3fba84c256cb6394a66a47e14b2289f4d8a4
Mark Henderson <henderson.mark@gmail.com>
2021-06-26 11:43:25 -0400

    feat(test): adding test for mint event

--- test/TokensFT.test.js
+++ test/TokensFT.test.js
@@ -32,9 +32,23 @@ describe('FTokens', function () {
   describe('FungibleTokens', function () {
     describe('Mint', function () {
       it('should increments the nextTokenId after each mint', async function () {
+        const oldBlock = await ethers.provider.getBlockNumber()
+
         const initialTokenId = await tokensF.nextTokenId()
         const mintFT = await tokensF.mint(owner.address, 1, [], txOptions)
         await mintFT.wait() // wait until transaction mined
+
+        const newBlock = await ethers.provider.getBlockNumber()
+
+        const filter = await tokensF.filters.Mint()
+        const events = await tokensF.queryFilter(filter)
+        const testEvents = events
+          .filter(block => oldBlock < block.blockNumber <= newBlock)
+
+        expect(testEvents.length).to.be.above(0)
+        expect(testEvents[0].event).to.equal('Mint')
+        expect(testEvents[0].eventSignature).to.equal('Mint(address,uint256,uint256,bytes)')
+
         const nextTokenId = await tokensF.nextTokenId()
         expect(nextTokenId).to.equal(initialTokenId.add(1))
       })
