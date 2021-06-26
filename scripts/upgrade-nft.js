22decf9e3852213625cb1ac57a12e5b3dc96fc68
geolffrey <gmjun2000@gmail.com>
2021-06-26 15:41:18 -0600

    test: upgradeable test

--- scripts/upgrade-nft.js
+++ scripts/upgrade-nft.js
@@ -1,15 +1,18 @@
 const { ethers, network, upgrades } = require('hardhat')
-const { getNFTContractAddress, runUpgradeTest } = require('../test/utils')
+const { getNFTContractAddress, runUpgradeTest, getCurrentVersion, txOptions } = require('../test/utils')
 
 async function main () {
-
   const NFToken = await ethers.getContractFactory('NFToken')
   const currentContract = getNFTContractAddress(network.name)
   const attachedContract = NFToken.attach(currentContract)
+  // Current contract
+  const id = await attachedContract.nextTokenId(txOptions)
+  const version = await getCurrentVersion(attachedContract)
+  console.log('>> Current version:', version);
 
   // Upgraded contract
   const upgradedNFT = await upgrades.upgradeProxy(currentContract, NFToken)
-  await runUpgradeTest(attachedContract, upgradedNFT)
+  await runUpgradeTest(attachedContract, {id, version})
 
   console.log(' > NFToken upgraded')
   process.stdout.write(upgradedNFT.address)
