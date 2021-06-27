fa6d4b4f64d74e5a6f4c590440ee0d2c3d6a4390
geolffrey <gmjun2000@gmail.com>
2021-06-26 22:43:34 -0600

    test: cleanup

--- scripts/upgrade-nft.js
+++ scripts/upgrade-nft.js
@@ -2,19 +2,38 @@ const { ethers, network, upgrades } = require('hardhat')
 const { getNFTContractAddress, runUpgradeTest, getCurrentVersion, txOptions } = require('../test/utils')
 
 async function main () {
-  const NFToken = await ethers.getContractFactory('NFToken')
+  const WatchitERC11155 = await ethers.getContractFactory('WatchitERC11155')
   const currentContract = getNFTContractAddress(network.name)
-  const attachedContract = NFToken.attach(currentContract)
+  const attachedContract = WatchitERC11155.attach(currentContract)
   // Current contract
   const id = await attachedContract.nextTokenId(txOptions)
   const version = await getCurrentVersion(attachedContract)
-  console.log('>> Current version:', version);
+  console.log('>> Current version:', version)
 
   // Upgraded contract
-  const upgradedNFT = await upgrades.upgradeProxy(currentContract, NFToken)
-  await runUpgradeTest(attachedContract, {id, version})
+  const upgradedNFT = await upgrades.upgradeProxy(currentContract, WatchitERC11155)
+  const newTokenId = await upgradedNFT.nextTokenId(txOptions)
 
-  console.log(' > NFToken upgraded')
+  // it('should retrieve a NFT previously minted', async function () {
+  if (id.toString() !== newTokenId.toString()) {
+    console.error('expected previously `nextTokenId` equal to upgraded contract state ')
+    process.exit(1)
+  } else {
+    console.log(' > nextTokenId state passed')
+  }
+
+  // it('should allow call added method `upgrade`', async function () {
+  const upgrade = await v2.upgrade(txOptions)
+  await upgrade.wait() // wait for tx
+  const newVersion = await v2.version(txOptions)
+  if (+version + 1 !== +newVersion) {
+    console.error('expected version to increment')
+    process.exit(1)
+  } else {
+    console.log(' > version state passed')
+  }
+
+  console.log(' > WatchitERC11155 upgraded')
   process.stdout.write(upgradedNFT.address)
 }
 
