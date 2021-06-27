f2ea4caf04a5b55553f2dba9ef0ff9a9be28f6e2
Mark Henderson <henderson.mark@gmail.com>
2021-06-27 00:23:35 -0400

    fix(contracts): removing redundant initializer check

--- contracts/WatchItERC20.sol
+++ contracts/WatchItERC20.sol
@@ -8,11 +8,7 @@ contract WatchItERC20 is ERC20Upgradeable {
   bool private initialized;
 
   function initialize(uint256 initialSupply) public initializer {
-    require(!initialized, "WatchItERC20 instance has already been initialized");
-
     ERC20Upgradeable.__ERC20_init("WatchIt", "WATCHIT");
-    initialized = true;
-
     _mint(msg.sender, initialSupply);
   }
 }
