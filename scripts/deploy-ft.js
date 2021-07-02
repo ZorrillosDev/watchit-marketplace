ab9bfa6cfc42bf2554fcfb2a24f9a15ac8ca45a4
Mark Henderson <mark@mrh.io>
2021-07-02 18:52:39 -0400

    chore: linting

--- scripts/deploy-ft.js
+++ scripts/deploy-ft.js
@@ -1,14 +1,14 @@
-const { ethers,network, upgrades } = require('hardhat')
+const { ethers, network, upgrades } = require('hardhat')
 
 async function main () {
   const WatchItERC20 = await ethers.getContractFactory('WatchItERC20')
   const WATCHIT = await upgrades.deployProxy(WatchItERC20, [200000], { initializer: 'initialize' })
-  const localNetwork = network.name ==='localhost';
+  const localNetwork = network.name === 'localhost'
 
   process.stdout.write(
-    localNetwork ?
-      WATCHIT.address :
-      `${network.name}:NFT:${WATCHIT.address}\n`
+    localNetwork
+      ? WATCHIT.address
+      : `${network.name}:NFT:${WATCHIT.address}\n`
   )
 }
 
--- scripts/upgrade-ft.js
+++ scripts/upgrade-ft.js
@@ -1,10 +1,10 @@
-const { ethers, network, upgrades } = require('hardhat')
-const { getFTContractAddress, runUpgradeTest, getCurrentVersion, txOptions } = require('../test/utils')
+const { ethers, network, upgrade } = require('hardhat') // eslint-disable-line
+const { getFTContractAddress, runUpgradeTest, getCurrentVersion, txOptions } = require('../test/utils') // eslint-disable-line
 
 async function main () {
   const WatchItERC20 = await ethers.getContractFactory('WatchItERC20')
   const currentContract = getFTContractAddress(network.name)
-  const attachedContract = WatchItERC20.attach(currentContract)
+  const attachedContract = WatchItERC20.attach(currentContract) // eslint-disable-line
   // Current contract
   // TODO testing upgrade ERC20
   // const version = await getCurrentVersion(attachedContract)
