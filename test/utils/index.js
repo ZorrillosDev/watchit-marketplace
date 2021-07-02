a21145402c1578889eb8714d3b05bab7c99aaadf
Mark Henderson <mark@mrh.io>
2021-07-02 17:38:31 -0400

    feat(tests): moving b58 <-> hex functions to utils

--- test/utils/index.js
+++ test/utils/index.js
@@ -1,7 +1,5 @@
-const txOptions = {
-  gasLimit: 8000000,
-  gasPrice: 1000000000
-}
+const bs58 = require('bs58')
+const { expect } = require('chai')
 
 function getFTContractAddress (networkName) {
   if (networkName === 'goerli') {
@@ -43,14 +41,29 @@ function getNFTContractAddress (networkName) {
   return process.env.LOCALHOST_CONTRACT_NFT
 }
 
+const bs58toHex = (b58) => `0x${Buffer.from(bs58.decode(b58).slice(2)).toString('hex')}`
+const hexToBs58 = (hex) => bs58.encode(Buffer.from(`1220${hex.slice(2)}`, 'hex'))
+
 async function getCurrentVersion (contract) {
   const hasVersionMethod = contract.version !== undefined
   return hasVersionMethod ? await contract.version() : 0
 }
 
 module.exports = {
+  bs58toHex,
+  hexToBs58,
   getFTContractAddress,
   getNFTContractAddress,
-  getCurrentVersion,
-  txOptions
+  getCurrentVersion
 }
+
+describe('Base 58 <--> Hex Conversion', function () {
+  const str = 'QmPXME1oRtoT627YKaDPDQ3PwA8tdP9rWuAAweLzqSwRST'
+
+  it('"round trips" from bs58 to hex and back', async () => {
+    const hex = bs58toHex(str)
+    const b58 = hexToBs58(hex)
+
+    expect(b58).to.equal(str)
+  })
+})
