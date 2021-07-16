edd687bd9fe985ade5a38238be67b717f40eb005
geolffrey <gmjun2000@gmail.com>
2021-07-16 13:00:14 -0600

    feat: add address after deploy

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -5,6 +5,11 @@ const root = process.cwd()
 const sourcePath = path.join(root, '.env')
 
 function writeInEnv (newData) {
+  /**
+   * Write or replace vars in .env
+   * @param {object}
+   * @type {Buffer}
+   */
   const currentEnvData = fs.readFileSync(sourcePath)
   const parsedEnv = parse(currentEnvData)
   const mergedData = Object.assign({}, parsedEnv, newData)
