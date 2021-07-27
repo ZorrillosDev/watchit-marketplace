8510606a43cc389c46606f179871e588ce9e936f
geolffrey <gmjun2000@gmail.com>
2021-07-27 12:34:25 -0600

    feat: routing setup

--- webpack/config/alias.js
+++ webpack/config/alias.js
@@ -5,7 +5,6 @@ export const aliasItems = {
   '@src': join(rootDir, '/src'),
   '@public': join(rootDir, '/public'),
   '@assets': join(rootDir, '/src/assets'),
-  '@styles': join(rootDir, '/src/styles'),
   '@components': join(rootDir, '/src/components'),
   '@pages': join(rootDir, '/src/pages'),
   '@helpers': join(rootDir, '/src/helpers')
