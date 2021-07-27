e2a2d993375d3ef2065b5948d207169a164d569f
geolffrey <gmjun2000@gmail.com>
2021-07-27 13:05:20 -0600

    fix: linting && type definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable  @typescript-eslint/explicit-function-return-type */
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
-import {Routing} from "@src/navigation/Routing";
+import { BrowserRouter } from 'react-router-dom'
+import { Routing } from '@src/navigation/Routing'
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routing/>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routing />
+    </BrowserRouter>
+  )
 }
 
 export default App
