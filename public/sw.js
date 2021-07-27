fea9bec3a9b376bf1ce29e1b0ec2eeb20f7ef42f
geolffrey <gmjun2000@gmail.com>
2021-07-27 11:25:14 -0600

    fix: lint

--- public/sw.js
+++ public/sw.js
@@ -23,9 +23,8 @@ const isLocalhost = Boolean(
 
 export function register (config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-
     window.addEventListener('load', () => {
-      const swUrl = `sw.js`
+      const swUrl = 'sw.js'
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
