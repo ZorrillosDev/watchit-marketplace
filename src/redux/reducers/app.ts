9657f499ae37eb426f0010b6e849f34f7b4a0ac8
geolffrey <gmjun2000@gmail.com>
2021-07-27 14:00:51 -0600

    fix: jest pages ref

---
+++ src/redux/reducers/app.ts
@@ -0,0 +1,31 @@
+import actions from '@redux/actions/app'
+
+interface Action {
+  payload: number
+  type: string
+}
+
+interface CounterState {
+  counter: number
+}
+
+const initialState: CounterState = {
+  counter: 0
+}
+
+export const appReducer = (state: CounterState = initialState, action: Action): object => {
+  switch (action.type) {
+    case actions.INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + action.payload
+      }
+    case actions.DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1
+      }
+    default:
+      return state
+  }
+}
