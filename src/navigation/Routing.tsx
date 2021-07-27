cb5e2150139832e1ce3371051de61d53ecf0f7fa
geolffrey <gmjun2000@gmail.com>
2021-07-27 13:05:20 -0600

    fix: linting && type definition

--- src/navigation/Routing.tsx
+++ src/navigation/Routing.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import Index from "@pages/Index";
-import {Route, Switch} from "react-router-dom";
-import {ROOT} from '@src/navigation/CONSTANTS'
+import React, { ReactElement } from 'react'
+import Index from '@pages/Index'
+import { Route, Switch } from 'react-router-dom'
+import { ROOT } from '@src/navigation/CONSTANTS'
 
-
-export const Routing = () => {
-    return (
-        <Switch>
-            <Route exact path={ROOT} component={Index}/>
-        </Switch>
-    )
+export const Routing = (): ReactElement => {
+  return (
+    <Switch>
+      <Route exact path={ROOT} component={Index} />
+    </Switch>
+  )
 }
