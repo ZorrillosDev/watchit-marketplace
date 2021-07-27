180c2ba2d1fff12e1b06d6d48ca7ca45d5681ea2
geolffrey <gmjun2000@gmail.com>
2021-07-27 14:05:33 -0600

    feat: redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable  @typescript-eslint/explicit-function-return-type */
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { Routing } from '@src/navigation/Routing'
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
+import {Routing} from '@src/navigation/Routing'
+import store from '@redux/store'
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routing />
-    </BrowserRouter>
-  )
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routing/>
+            </BrowserRouter>
+        </Provider>
+    )
 }
 
 export default App
