fe1e55148b40841e3ade278dcb8c0e85e30bbfbc
geolffrey <gmjun2000@gmail.com>
2021-07-28 13:34:34 -0600

    chore: redux devtools setup

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,23 +3,27 @@ import {routerMiddleware} from 'connected-react-router/immutable'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import {History} from 'history'
-import {fromJS} from 'immutable'
 
 // @ts-ignore
 export default (history: History, initialState = {}): Store => {
+    // devtools for debugging in dev environment.
+    // @ts-ignore
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
     // Array of all middlewares to be applied.
     const middlewares = [
         thunk,
         routerMiddleware(history)
     ]
 
-    // devtools for debugging in dev environment.
-    // @ts-ignore
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-    const enhancers = applyMiddleware(...middlewares)
+    const enhancers = [
+        applyMiddleware(...middlewares)
+    ]
+
 
     return createStore(
         rootReducer(history),
-        composeEnhancers(enhancers)
+        initialState,
+        composeEnhancers(...enhancers)
     )
 }
