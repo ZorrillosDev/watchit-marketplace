5d6269ad6ab4b26e4348ab5cfbfbfc36e883380f
geolffrey <gmjun2000@gmail.com>
2021-08-06 17:31:39 -0600

    docs: jest link to fileTransformer.js

--- .jest/fileTransformer.js
+++ .jest/fileTransformer.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+//https://jestjs.io/docs/webpack
 module.exports = {
   process(src, filename, config, options) {
     return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
