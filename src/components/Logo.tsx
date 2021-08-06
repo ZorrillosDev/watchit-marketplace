7a6642fe9cbd3b54cd9cfdcd5475345ecb845c77
geolffrey <gmjun2000@gmail.com>
2021-08-06 13:12:19 -0600

    feat: add logo and wallet button

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import LogoPng from '@assets/icons/icon.png'
+
+const Logo = styled.img`
+  border-radius: 50px;
+  max-width: 40px;
+`
+
+Logo.defaultProps = {
+    src: LogoPng
+}
+export default Logo
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as TopHeader } from '@components/TopHeader'
 export {default as MainSlider} from '@components/MainSlider'
+export {default as RoundButton} from '@components/RoundButton'
+export {default as Logo} from '@components/Logo'
