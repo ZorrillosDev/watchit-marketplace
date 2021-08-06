1279bad3652f084253f2c7288d992bd0e1a0dd6e
geolffrey <gmjun2000@gmail.com>
2021-08-06 17:06:35 -0600

    refactor: split to WalletButton.tsx

---
+++ src/components/WalletButton.tsx
@@ -0,0 +1,15 @@
+import i18n from "@src/i18n";
+import React, {FunctionComponent, ReactElement} from "react";
+import {RoundButton} from "@components/index";
+import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
+
+const WalletButton: FunctionComponent = (): ReactElement => (
+    <RoundButton
+        variant={'contained'} color={'secondary'}
+        startIcon={<AccountBalanceWalletIcon fontSize={'inherit'}/>}
+    >
+        {i18n.t('GLOBAL_WALLET')}
+    </RoundButton>
+)
+
+export default WalletButton;
