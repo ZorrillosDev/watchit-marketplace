e61a5cc8ca247a5ef5464ef665087cb6e8f9b756
geolffrey <gmjun2000@gmail.com>
2021-08-09 10:17:20 -0600

    test: add default translation test

---
+++ src/components/__tests__/WalletButton.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import WalletButton from '@components/WalletButton'
+import 'jest-styled-components'
+
+
+/* eslint-disable no-undef */
+describe('WalletButton component', () => {
+        it('should render', () => {
+            const walletButtonComponent = shallow(<WalletButton/>)
+            expect(walletButtonComponent).toMatchSnapshot()
+        })
+
+        it('should display english translation', () => {
+            const walletButtonComponent = shallow(<WalletButton/>)
+            expect(walletButtonComponent.dive().text()).toBe('Wallet')
+        })
+    }
+)
