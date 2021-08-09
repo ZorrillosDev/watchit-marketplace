dc6a7bfcd7ff11973bb156ab96ac06a90447037f
geolffrey <gmjun2000@gmail.com>
2021-08-09 10:17:06 -0600

    refactor: add i18n context

--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -1,15 +1,17 @@
-import i18n from '@src/i18n'
-import React, { FunctionComponent, ReactElement } from 'react'
-import { RoundButton } from '@components/index'
+import {useTranslation} from 'react-i18next';
+import React, {FunctionComponent, ReactElement} from 'react'
+import {RoundButton} from '@components/index'
 import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet'
 
-const WalletButton: FunctionComponent = (): ReactElement => (
-  <RoundButton
-    variant='contained' color='secondary'
-    startIcon={<AccountBalanceWalletIcon fontSize='inherit' />}
-  >
-    {i18n.t('GLOBAL_WALLET')}
-  </RoundButton>
-)
+const WalletButton: FunctionComponent = (props: any): ReactElement => {
+    const {t} = useTranslation()
+    return <RoundButton
+        variant='contained' color='secondary'
+        startIcon={<AccountBalanceWalletIcon fontSize='inherit'/>}
+    >
+        {t('GLOBAL_WALLET')}
+    </RoundButton>
+}
+
 
 export default WalletButton
