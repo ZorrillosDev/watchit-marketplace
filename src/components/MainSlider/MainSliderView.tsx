b2c6e7b7c7a0a36ffd8ed36d380534f801401f63
jadapema <Ruperma040193>
2021-09-17 07:14:23 -0600

    feat: add swiper

--- src/components/MainSlider/MainSliderView.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from '@material-ui/core'
-import styled from 'styled-components'
-
-const MainSliderView = styled(Box)`
-  width: 100%;
-  margin-bottom: 1rem;
-  position: relative;
-`
-export default MainSliderView
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import MainSlider from '@src/components/MainSlider'
+import React, { ReactElement } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const Home = (): JSX.Element => {
-  return <MainSlider />
+  return <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+  >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+  </Swiper>
 }
 
 export default Home
