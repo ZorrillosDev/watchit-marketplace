c47f73bb9c7509cddde34115c09372ad7f5a1bf8
jadapema <Ruperma040193>
2021-11-04 17:12:28 -0600

    feat: added landing mos loved section

--- src/pages/Home/components/HomeMostLoved.tsx
+++ src/pages/Home/components/HomeMostLoved.tsx
@@ -1,106 +1,99 @@
 // REACT IMPORTS
 import React, { FC } from 'react'
 
+// THIRD PARTY IMPORTS
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+import SwiperCore, {
+  Pagination
+} from 'swiper'
+
 // MUI IMPORTS
-import { Grid, Typography, Container } from '@mui/material'
+import {Grid, Typography, Container, useMediaQuery, styled, Box, BoxProps, ContainerProps} from '@mui/material'
+import {useTheme} from "@mui/material/styles";
+
+// PROJECT IMPORTS
 import Poster from '@components/Poster'
 
 // ===========================|| HOME - TRENDING ||=========================== //
 
-const HomeTrending: FC = (): JSX.Element => {
+SwiperCore.use([Pagination])
+
+const HomeMostLoved: FC = (): JSX.Element => {
+  const theme = useTheme()
+  let itemCount = 1
+  itemCount = useMediaQuery(theme.breakpoints.up('sm')) ? 3 : itemCount
+  itemCount = useMediaQuery(theme.breakpoints.up('md')) ? 4 : itemCount
+  itemCount = useMediaQuery(theme.breakpoints.up('lg')) ? 7 : itemCount
+  itemCount = useMediaQuery(theme.breakpoints.up('xl')) ? 12 : itemCount
+
   return (
-    <Container>
-      <Grid container alignItems='center' justifyContent='space-between' spacing={6}>
+    <HomeMostLovedWrapper>
+      <Grid container alignItems='center' justifyContent='space-between' spacing={3}>
         <Grid item xs={12} md={10} lg={6}>
           <Typography
             variant='h3'
             color='primary.dark'
           >
-            NTF MOVIES IN TRENDING
+            MOST LOVED MOVIES
           </Typography>
         </Grid>
         <Grid item xs={12}>
-          <Grid container spacing={3}>
+          <Swiper
+            slidesPerView={itemCount}
+            spaceBetween={16}
+            slidesPerGroup={1}
+            loop
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false
+            }}
+            pagination={{
+              clickable: true
+            }}
+          >
             {
-              fakeMovies.map((poster) => {
-                return (
-                  <Grid item xs={12} sm={4} md={3} lg={2} xl={1} key={poster.title}>
-                    <Poster {...poster} />
-                  </Grid>
-                )
-              })
+              fakeMovies.map((poster) => (
+                <SwiperSlide key={poster.title}>
+                  <Poster {...poster} />
+                </SwiperSlide>
+              ))
             }
-          </Grid>
+          </Swiper>
         </Grid>
       </Grid>
-    </Container>
+    </HomeMostLovedWrapper>
   )
 }
 
-export default HomeTrending
+export default HomeMostLoved
+
+export const HomeMostLovedWrapper = styled(Container)<ContainerProps>(({ theme }) => ({
+  '.swiper-container': {
+    overflow: 'visible'
+  },
+  '.swiper-slide': {
+    paddingTop: '1rem',
+    paddingBottom: '4rem'
+  },
+  '& .swiper-pagination': {
+    bottom: '1rem',
+    '& .swiper-pagination-bullet': {
+      width: '0.7rem',
+      height: '0.7rem',
+      backgroundColor: theme.palette.primary.main,
+      transition: 'all 0.5s ease-in-out'
+    },
+    '& .swiper-pagination-bullet-active': {
+      width: '2rem !important',
+      borderRadius: '8px'
+    }
+  }
+}))
 
 // TODO delete this when data comes from backend
 const fakeMovies = [
-  {
-    creator: {
-      username: '@gmena',
-      profileUrl: 'https://mui.com/static/images/avatar/1.jpg'
-    },
-    owner: {
-      username: '@jadapema',
-      profileUrl: 'https://mui.com/static/images/avatar/2.jpg'
-    },
-    posterUrl: 'https://cuevana3.io/wp-content/uploads/2021/11/la-novena-profecia-50825-poster-211x300.jpg',
-    value: 2.4,
-    title: 'La novena profecía',
-    favoriteCount: 23,
-    isFavorite: true
-  },
-  {
-    creator: {
-      username: '@gmena',
-      profileUrl: 'https://mui.com/static/images/avatar/1.jpg'
-    },
-    owner: {
-      username: '@jadapema',
-      profileUrl: 'https://mui.com/static/images/avatar/2.jpg'
-    },
-    posterUrl: 'https://cuevana3.io/wp-content/uploads/2021/11/help-50817-poster-200x300.jpg',
-    value: 1.8,
-    title: 'Help',
-    favoriteCount: 20,
-    isFavorite: false
-  },
-  {
-    creator: {
-      username: '@gmena',
-      profileUrl: 'https://mui.com/static/images/avatar/1.jpg'
-    },
-    owner: {
-      username: '@jadapema',
-      profileUrl: 'https://mui.com/static/images/avatar/2.jpg'
-    },
-    posterUrl: 'https://cuevana3.io/wp-content/uploads/2021/08/safer-at-home-46847-poster-200x300.jpg',
-    value: 1.8,
-    title: 'Safer at Home',
-    favoriteCount: 20,
-    isFavorite: true
-  },
-  {
-    creator: {
-      username: '@gmena',
-      profileUrl: 'https://mui.com/static/images/avatar/4.jpg'
-    },
-    owner: {
-      username: '@jadapema',
-      profileUrl: 'https://mui.com/static/images/avatar/2.jpg'
-    },
-    posterUrl: 'https://cuevana3.io/wp-content/uploads/2021/11/13-minutes-50881-poster-200x300.jpg',
-    value: 1.8,
-    title: '13 Minutes',
-    favoriteCount: 20,
-    isFavorite: false
-  },
   {
     creator: {
       username: '@gmena',
@@ -168,11 +161,71 @@ const fakeMovies = [
     },
     owner: {
       username: '@jadapema',
+      profileUrl: 'https://mui.com/static/images/avatar/2.jpg'
+    },
+    posterUrl: 'https://cuevana3.io/wp-content/uploads/2021/11/la-novena-profecia-50825-poster-211x300.jpg',
+    value: 2.4,
+    title: 'La novena profecía',
+    favoriteCount: 23,
+    isFavorite: true
+  },
+  {
+    creator: {
+      username: '@gmena',
+      profileUrl: 'https://mui.com/static/images/avatar/1.jpg'
+    },
+    owner: {
+      username: '@jadapema',
       profileUrl: 'https://mui.com/static/images/avatar/4.jpg'
     },
     posterUrl: 'https://cuevana3.io/wp-content/uploads/2021/11/dont-look-there-50829-poster-200x300.jpg',
     value: 1.8,
     title: 'Don’t Look There',
+    favoriteCount: 20,
+    isFavorite: false
+  },
+  {
+    creator: {
+      username: '@gmena',
+      profileUrl: 'https://mui.com/static/images/avatar/1.jpg'
+    },
+    owner: {
+      username: '@jadapema',
+      profileUrl: 'https://mui.com/static/images/avatar/2.jpg'
+    },
+    posterUrl: 'https://cuevana3.io/wp-content/uploads/2021/11/help-50817-poster-200x300.jpg',
+    value: 1.8,
+    title: 'Help',
+    favoriteCount: 20,
+    isFavorite: false
+  },
+  {
+    creator: {
+      username: '@gmena',
+      profileUrl: 'https://mui.com/static/images/avatar/1.jpg'
+    },
+    owner: {
+      username: '@jadapema',
+      profileUrl: 'https://mui.com/static/images/avatar/2.jpg'
+    },
+    posterUrl: 'https://cuevana3.io/wp-content/uploads/2021/08/safer-at-home-46847-poster-200x300.jpg',
+    value: 1.8,
+    title: 'Safer at Home',
+    favoriteCount: 20,
+    isFavorite: true
+  },
+  {
+    creator: {
+      username: '@gmena',
+      profileUrl: 'https://mui.com/static/images/avatar/4.jpg'
+    },
+    owner: {
+      username: '@jadapema',
+      profileUrl: 'https://mui.com/static/images/avatar/2.jpg'
+    },
+    posterUrl: 'https://cuevana3.io/wp-content/uploads/2021/11/13-minutes-50881-poster-200x300.jpg',
+    value: 1.8,
+    title: '13 Minutes',
     favoriteCount: 20,
     isFavorite: false
   },
