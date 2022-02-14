f376dd1ce8bf58f2d85c36e9661461fd35f1c5ee
gmena <gmjun2000@gmail.com>
2022-02-14 09:21:57 -0600

    feat: validate metamask connected wallet for movie upload

--- src/pages/Create/MovieCreateContainer.tsx
+++ src/pages/Create/MovieCreateContainer.tsx
@@ -1,39 +1,47 @@
 // react imports
-import React, { FC } from 'react'
+import React, {FC} from 'react'
 
 // project imports
 
-import { MovieCreateView } from '@pages/Create/MovieCreateView'
-import { MoviesActions, MoviesState } from '@state/movies/types'
-import { commitUploadMovie } from '@state/movies/actions'
-import { connect, RootStateOrAny } from 'react-redux'
-import { selectUploadProgress } from '@state/movies/selector'
+import {MovieCreateView} from '@pages/Create/MovieCreateView'
+import {MoviesActions, MoviesState} from '@state/movies/types'
+import {commitUploadMovie} from '@state/movies/actions'
+import {connect, RootStateOrAny} from 'react-redux'
+import {selectUploadProgress} from '@state/movies/selector'
+import {useEthers} from "@usedapp/core";
 
 /* eslint-disable  @typescript-eslint/consistent-type-assertions */
 
 // ===========================|| MOVIE - CREATE - CONTAINER ||=========================== //
 
-export const MovieCreateContainer: FC<MoviesState & MoviesActions> = ({ commitUploadMovie, progress }): JSX.Element => {
-  const onSubmit = (values: any): void => {
-    const formData = new FormData()
+export const MovieCreateContainer: FC<MoviesState & MoviesActions> = ({commitUploadMovie, progress}): JSX.Element => {
+    const {account} = useEthers()
 
-    for (const [idx, value] of Object.entries(values)) {
-      formData.append(idx, value as any)
+    const onSubmit = (values: any): void => {
+        const formData = new FormData()
+        // Avoid request if wallet isn't connected
+        if (account === null)
+            return;
+
+        for (const [idx, value] of Object.entries(values)) {
+            formData.append(idx, value as any)
+        }
+
+        // Dispatch action api call with creator append from wallet
+        formData.append('creator', account ?? '')
+        commitUploadMovie(formData)
     }
-    // Here call the api
-    commitUploadMovie(formData)
-  }
 
-  return <MovieCreateView {...{ onSubmit, progress }} />
+    return <MovieCreateView {...{onSubmit, progress}} />
 }
 
-const mapDispatchToProps: Partial<MoviesActions> = { commitUploadMovie }
+const mapDispatchToProps: Partial<MoviesActions> = {commitUploadMovie}
 const mapStateToProps = (state: RootStateOrAny): Partial<MoviesState> => {
-  const progress = selectUploadProgress(state)
-  return { progress }
+    const progress = selectUploadProgress(state)
+    return {progress}
 }
 
 export const MovieCreate = connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(React.memo(MovieCreateContainer))
