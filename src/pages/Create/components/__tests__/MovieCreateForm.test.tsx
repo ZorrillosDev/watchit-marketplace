d1eaee9f78ad777cac3b676ef126b62224852b33
gmena <gmjun2000@gmail.com>
2022-02-20 09:12:26 -0600

    fix: tests

--- src/pages/Create/components/__tests__/MovieCreateForm.test.tsx
+++ src/pages/Create/components/__tests__/MovieCreateForm.test.tsx
@@ -89,13 +89,6 @@ describe('<MovieCreateForm />', () => {
   })
 
 
-  it('should have movie create form "nft desc" with valid helper text', () => {
-    const translate = i18n.t('MOVIE_CREATE_NFT_DESC_HELP_TEXT')
-    const component = mount(<MovieCreateForm onSubmit={onSubmit} poster='fer' film='efr' {...functions} />)
-    const label = component.find('.MuiFormHelperText-root').at(6)
-    expect(label.text()).toMatch(translate)
-  })
-
   it('should have movie create form "button" with valid text', () => {
     const translate = i18n.t('MOVIE_CREATE_ADD_BUTTON')
     const component = mount(<MovieCreateForm onSubmit={onSubmit} poster='' film='' {...functions} />)
