01cb7925ac3d9a8bbb55fc478af7e77edf5045fd
gmena <gmjun2000@gmail.com>
2022-02-24 11:06:54 -0600

    fix: test for token movie

--- src/pages/Movie/components/MovieProfilePrice/__tests__/MovieProfilePrice.test.tsx
+++ src/pages/Movie/components/MovieProfilePrice/__tests__/MovieProfilePrice.test.tsx
@@ -1,25 +1,35 @@
 import React from 'react'
-import { mount, shallow } from 'enzyme'
+import {mount, shallow} from 'enzyme'
 import MovieProfilePrice from '@pages/Movie/components/MovieProfilePrice'
 import i18n from '@src/i18n'
-import { Typography } from '@mui/material'
-import { Movie } from '@state/movies/types'
+import {Typography} from '@mui/material'
+import {Movie} from '@state/movies/types'
+import {store} from '@state/store'
+import {Provider} from 'react-redux'
 
 /* eslint-disable  @typescript-eslint/consistent-type-assertions */
 
 describe('<MovieProfilePrice />', () => {
   const movie = {
-    creator: "0xEe99CeFF640d37Edd9cac8c7cfF4Ed4cD609f435",
-    token: "651268735865169554630764440897259763067310892845464250243992889613221274545"
+    creator: '0xEe99CeFF640d37Edd9cac8c7cfF4Ed4cD609f435',
+    token: '651268735865169554630764440897259763067310892845464250243992889613221274545'
   } as Movie
   it('should render', () => {
-    const component = shallow(<MovieProfilePrice {...movie} />)
+    const component = shallow(
+        <Provider store={store}>
+          <MovieProfilePrice {...movie} />
+        </Provider>
+    )
     expect(component).toMatchSnapshot()
   })
 
   it('should have movie profile price valid higher price text', () => {
     const translate = i18n.t('MOVIE_PROFILE_PRICE_HIGHER')
-    const component = mount(<MovieProfilePrice {...movie} />)
+    const component = mount(
+        <Provider store={store}>
+          <MovieProfilePrice {...movie} />
+        </Provider>
+    )
     const typography = component.find(Typography).at(0)
 
     expect(typography.text()).toContain(translate)
@@ -27,7 +37,11 @@ describe('<MovieProfilePrice />', () => {
 
   it('should have movie profile price valid price text', () => {
     const price = 0
-    const component = mount(<MovieProfilePrice {...movie} price={price} />)
+    const component = mount(
+        <Provider store={store}>
+          <MovieProfilePrice {...movie} price={price}/>
+        </Provider>
+    )
     const typography = component.find(Typography).at(1)
 
     expect(typography.text()).toContain(`${price} ETH`)
@@ -35,7 +49,11 @@ describe('<MovieProfilePrice />', () => {
 
   it('should have movie profile price valid owner text', () => {
     const translate = i18n.t('MOVIE_PROFILE_OWNER')
-    const component = mount(<MovieProfilePrice {...movie} />)
+    const component = mount(
+        <Provider store={store}>
+          <MovieProfilePrice {...movie} />
+        </Provider>
+    )
     const typography = component.find(Typography).at(3)
 
     expect(typography.text()).toContain(translate)
