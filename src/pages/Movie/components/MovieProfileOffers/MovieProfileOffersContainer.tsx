142d8373c6e23435ee077a9d438cb93b01e51140
gmena <gmjun2000@gmail.com>
2022-02-28 15:11:33 -0600

    fix: unsupported chainId

--- src/pages/Movie/components/MovieProfileOffers/MovieProfileOffersContainer.tsx
+++ src/pages/Movie/components/MovieProfileOffers/MovieProfileOffersContainer.tsx
@@ -1,12 +1,13 @@
 // REACT IMPORTS
-import React, { FC, useEffect } from 'react'
+import React, {FC, useEffect} from 'react'
 
 // PROJECT IMPORTS
-import { MovieProfileOffersView } from '@pages/Movie/components/MovieProfileOffers/MovieProfileOffersView'
-import { MoviesActions, MoviesState } from '@state/movies/types'
-import { connect, RootStateOrAny } from 'react-redux'
-import { fetchRecentMovieBids } from '@state/movies/actions'
-import { selectBidCollection } from '@state/movies/selector'
+import {MovieProfileOffersView} from '@pages/Movie/components/MovieProfileOffers/MovieProfileOffersView'
+import {MovieArgs, MoviesActions, MoviesState} from '@state/movies/types'
+import {connect, RootStateOrAny} from 'react-redux'
+import {fetchRecentMovieBids} from '@state/movies/actions'
+import {selectBidCollection} from '@state/movies/selector'
+import {useParams} from "react-router";
 
 /* eslint-disable  @typescript-eslint/consistent-type-assertions */
 
@@ -14,25 +15,27 @@ import { selectBidCollection } from '@state/movies/selector'
 
 type MovieProfileOffersProps = MoviesActions & MoviesState
 const MovieProfileOffersContainer: FC<MovieProfileOffersProps> = (props): JSX.Element => {
-  const {
-    fetchRecentMovieBids,
-    bidCollection
-  } = props
+    const {id} = useParams<MovieArgs>()
+    const {
+        fetchRecentMovieBids,
+        bidCollection
+    } = props
 
-  useEffect(() => {
-    fetchRecentMovieBids()
-  }, [])
 
-  return <MovieProfileOffersView rows={bidCollection} />
+    useEffect(() => {
+        fetchRecentMovieBids({id})
+    }, [bidCollection.length])
+
+    return <MovieProfileOffersView rows={bidCollection}/>
 }
 
-const mapDispatchToProps: Partial<MoviesActions> = { fetchRecentMovieBids }
+const mapDispatchToProps: Partial<MoviesActions> = {fetchRecentMovieBids}
 const mapStateToProps = (state: RootStateOrAny): Partial<MoviesState> => {
-  const bidCollection = selectBidCollection(state)
-  return { bidCollection }
+    const bidCollection = selectBidCollection(state)
+    return {bidCollection}
 }
 
 export const MovieProfileOffers = connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(MovieProfileOffersContainer)
--- src/redux/movies/actions.ts
+++ src/redux/movies/actions.ts
@@ -27,10 +27,10 @@ export const fetchRecentMovies = <P extends MoviesArgs>(params?: P): ThunkAction
   }
 }
 
-export const fetchRecentMovieBids = <P extends MoviesArgs>(params?: P): ThunkAction<Promise<void>> => {
+export const fetchRecentMovieBids = <P extends MovieArgs>(params: P): ThunkAction<Promise<void>> => {
   return async (dispatch: ThunkDispatcher) => {
     try {
-      const moviesBids: MovieBid[] = await request(Endpoints.bid, { params })
+      const moviesBids: MovieBid[] = await request(Endpoints.bid, {params})
       dispatch(setBidsToMovie(moviesBids))
     } catch (e) {
       // TODO handle error here
