4b5c3edd7a14c15e48037c054543b684484e3539
gmena <gmjun2000@gmail.com>
2022-03-12 11:35:49 -0600

    Merge branch 'main' into accept/offer/result

--- src/redux/__tests__/movies.tests.ts
+++ src/redux/__tests__/movies.tests.ts
@@ -1,26 +1,39 @@
 import { ThunkDispatcher, ThunkAction } from '@state/types'
-import { Movie } from '@state/movies/types/movies'
+import { Movie, MovieBid } from '@state/movies/types/movies'
 import { FAKE_MOVIES } from '@src/config'
 import { request } from '@state/service'
 import { MovieBidArgs } from '@state/movies/types'
-import { fetchRecentMovies, fetchRecentMovieBids, commitBidMovie, commitUploadMovie } from '@state/movies/actions'
-import reducer, { addMovie, initialState, setMovies, setMovie } from '@state/movies/reducer'
+import { fetchRecentMovies, fetchRecentMovieBids, commitBidMovie, commitUploadMovie, flushBidsForMovie } from '@state/movies/actions'
+import reducer, { addMovie, initialState, setMovies, setMovie, setUploadProgress, setBidsToMovie, addBidToMovie } from '@state/movies/reducer'
 
 jest.mock('@state/service')
 
 describe('Movies store', () => {
   let movies: Movie[]
+  let movieBid: MovieBid 
+  let movieBids: MovieBid[]
   let dispatch: ThunkDispatcher
   let getState: () => unknown
   let actionForFetchRecent: ThunkAction<void>
   let actionForFetchRecentBids: ThunkAction<void>
   let actionForCommitBidMovie: ThunkAction<void>
   let actionForCommitUploadMovie: ThunkAction<void>
+  let actionForFlushBidsForMovie: ThunkAction<void>
   let bidMovieArgs: MovieBidArgs
+  let bidFlushMovieArgs: Omit<MovieBidArgs, 'bid'>
 
   beforeAll(() => {
     // @typescript-eslint/consistent-type-assertions
     movies = FAKE_MOVIES as unknown as Movie[]
+    movieBids = [{
+      id: 'tt00',
+      account: "0x0",
+      created_at: '00/00/00',
+      bid: 10000
+    }] as unknown as MovieBid[]
+
+    movieBid = movieBids[0]
+
   })
 
   it('should return tracking initial state', () => {
@@ -59,16 +72,48 @@ describe('Movies store', () => {
     })
   })
 
+  it('should handle set for bid collection', () => {
+    const current = reducer(initialState, setBidsToMovie(movieBids));
+    expect(current).toEqual({ ...initialState, ...{ bidCollection: movieBids } })
+    expect(reducer(current, setBidsToMovie(movieBids))).toEqual({
+      ...initialState,
+      bidCollection: movieBids
+    })
+  })
+
+  it('should handle add to bid collection', () => {
+    const current = reducer(initialState, addBidToMovie(movieBid));
+    expect(current).toEqual({ ...initialState, ...{ bidCollection: [movieBid] } })
+    expect(reducer(current, addBidToMovie(movieBid))).toEqual({
+      ...initialState,
+      bidCollection: [movieBid, movieBid]
+    })
+  })
+
+  it('should handle set for upload progress', () => {
+    const current = reducer(initialState, setUploadProgress(10))
+
+    expect(current).toEqual({ ...initialState, ...{ progress: 10 } })
+    expect(reducer(current, setUploadProgress(15))).toEqual({
+      ...initialState,
+      progress: 15
+    })
+  })
+
+
   describe('thunk', () => {
     beforeEach(() => {
       // initialize new spies
       dispatch = jest.fn()
       getState = jest.fn()
       bidMovieArgs = { bid: 2, account: 'test', id: '1' }
+      bidFlushMovieArgs = { id: '1', 'account': '0x0' }
+
       actionForFetchRecent = fetchRecentMovies()
       actionForFetchRecentBids = fetchRecentMovieBids({ id: '1' })
       actionForCommitBidMovie = commitBidMovie(bidMovieArgs)
       actionForCommitUploadMovie = commitUploadMovie({} as any)
+      actionForFlushBidsForMovie = flushBidsForMovie(bidFlushMovieArgs)
     })
 
     it('should call recent action with valid args ', async () => {
@@ -86,9 +131,16 @@ describe('Movies store', () => {
       expect(request).toHaveBeenCalledWith('/movie/bid?id=1', { data: bidMovieArgs, method: 'post' })
     })
 
+    it('should call flush bids for movie action with valid args ', async () => {
+      await actionForFlushBidsForMovie(dispatch, getState, undefined)
+      expect(request).toHaveBeenCalledWith('/movie/bid/flush?id=1', { data: bidFlushMovieArgs, method: 'post' })
+    })
+
     it('should call commit upload movie action with valid args ', async () => {
       await actionForCommitUploadMovie(dispatch, getState, undefined)
       expect(request).toBeCalled()
     })
   })
 })
+
+
