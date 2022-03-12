3d9eb7a841c1ca80b39bc4ba707a8334b99cca52
gmena <gmjun2000@gmail.com>
2022-03-12 13:47:08 -0600

    test: safePurchase

--- src/redux/__tests__/web3.tests.ts
+++ src/redux/__tests__/web3.tests.ts
@@ -1,17 +1,15 @@
 import { ThunkDispatcher, ThunkAction } from '@state/types'
-// import { Movie } from '@state/movies/types/movies'
-// import { FAKE_MOVIES } from '@src/config'
-// import { request } from '@state/service'
-import { Web3SetApprovalForArgs } from '@state/web3/types'
-import { setApprovalFor } from '@state/web3/actions'
+import { Web3SetApprovalForArgs, Web3SafePurchaseArgs } from '@state/web3/types'
+import { setApprovalFor, safePurchase } from '@state/web3/actions'
 import { callSafePurchase, callSetApprovalFor } from '@w3/calls/nft'
-// import reducer, { addMovie, initialState, setMovies, setMovie } from '@state/movies/reducer'
 
 jest.mock('@w3/calls/nft')
 
 describe('Web3 store', () => {
   let actionForSetCallResult: ThunkAction<void>
+  let actionForSafePurchase: ThunkAction<void>
   let setApprovalArgs: Web3SetApprovalForArgs
+  let safePurchaseArgs: Web3SafePurchaseArgs
   let dispatch: ThunkDispatcher
   let getState: () => unknown
 
@@ -19,12 +17,19 @@ describe('Web3 store', () => {
     // initialize new spies
     dispatch = jest.fn()
     getState = jest.fn()
+    safePurchaseArgs = { tokenId: '0x000' }
     setApprovalArgs = { tokenId: '0x000', approved: '100', operator: '0x0' }
     actionForSetCallResult = setApprovalFor(setApprovalArgs)
+    actionForSafePurchase = safePurchase(safePurchaseArgs)
   })
 
   it('should call approvalFor with valid args ', async () => {
     await actionForSetCallResult(dispatch, getState, undefined)
     expect(callSetApprovalFor).toHaveBeenCalledWith(setApprovalArgs)
+  })
+
+  it('should call safePurchase with valid args ', async () => {
+    await actionForSafePurchase(dispatch, getState, undefined)
+    expect(callSafePurchase).toHaveBeenCalledWith(safePurchaseArgs)
   })
 })
