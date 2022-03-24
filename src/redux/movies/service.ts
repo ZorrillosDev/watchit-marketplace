33c4ddb4e08a4af9238e4e0086a73de356d22df8
geolffreym <gmjun2000@gmail.com>
2022-03-24 14:58:44 -0600

    refactor: high cohesion for movie states

--- src/redux/movies/service.ts
+++ src/redux/movies/service.ts
@@ -1,7 +1,5 @@
 export enum Endpoints {
   recent = '/movie/recent',
   profile = '/movie/profile',
-  create = '/movie/create',
-  bid = '/movie/bid',
-  bidsFlush = '/movie/bid/flush',
+  create = '/movie/create'
 }
