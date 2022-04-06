70ce82b54ff77f99090b5d9c97e81351cc0bb4ec
geolffreym <gmjun2000@gmail.com>
2022-04-06 11:26:44 -0600

    fix: status response

--- src/redux/service.ts
+++ src/redux/service.ts
@@ -5,13 +5,10 @@ export const request = async (path: string, args?: object): Promise<any> => {
   const res = await axios.request({
     url: path,
     baseURL: API_ENDPOINT,
-    ...args,
-    headers: {
-      
-    },
+    ...args
   })
-  
-  return res.status === 200
+
+  return res.status >= 200 && res.status < 300
     ? await Promise.resolve(res.data)
     : await Promise.reject(res)
 }
